3e5d5aaeed994ff5f691fa4e8ed7e6f94669ac7a
akshaypatil581 <akshaypatil581@gmail.com>
2020-09-21 21:26:35 -0700

    Started working on Registration Activity

--- app/src/main/java/com/example/whatsappcone/Registration.java
+++ app/src/main/java/com/example/whatsappcone/Registration.java
@@ -1,14 +1,86 @@
 package com.example.whatsappcone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class Registration extends AppCompatActivity {
+
+    // Widgets
+    EditText userET, passET, emailET;
+    Button registerBtn;
+
+    // Firebase
+    FirebaseAuth auth;
+    DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        // Initializing Widgets
+        userET = findViewById(R.id.userEditText);
+        passET = findViewById(R.id.passEditText);
+        emailET = findViewById(R.id.emailEditText);
+        registerBtn = findViewById(R.id.buttonRegister);
+
+        // Firebase Auth
+        auth = FirebaseAuth.getInstance();
+
+    }
+
+    private void RegisterNow(final String username, String email, String password) {
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser firebaseUser = auth.getCurrentUser();
+                            String userid = firebaseUser.getUid();
+
+                            myRef = FirebaseDatabase.getInstance().getReference("MyUsers")
+                                    .child(userid);
+
+                            HashMap<String, String> hashMap = new HashMap<>();
+                            hashMap.put("id", userid);
+                            hashMap.put("username", username);
+                            hashMap.put("imageURL", "default");
+
+                            // Openning the main activity after successful longin
+
+                            myRef.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Intent i = new Intent(Registration.this, MainActivity.class);
+                                        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(i);
+                                        finish();
+                                    }
+                                }
+                            });
+
+
+                        } else {
+                            Toast.makeText(Registration.this, "Invalid Email or Password", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
