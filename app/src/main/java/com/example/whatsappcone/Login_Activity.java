ca1fa6a37de4c3393858e18a9709b80962901e98
akshaypatil581 <akshaypatil581@gmail.com>
2020-09-22 18:16:18 -0700

    Created login activity with firebase authentication for

--- app/src/main/java/com/example/whatsappcone/Login_Activity.java
+++ app/src/main/java/com/example/whatsappcone/Login_Activity.java
@@ -1,14 +1,72 @@
 package com.example.whatsappcone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Text;
 
 public class Login_Activity extends AppCompatActivity {
+
+    EditText userETLogin, passETLogin;
+    Button LoginBtn;
+
+    // Firebase:
+    FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_);
+
+        userETLogin = findViewById(R.id.editTextLogin);
+        passETLogin = findViewById(R.id.editText3);
+        LoginBtn = findViewById(R.id.buttonLogin);
+
+        //Firebase Auth:
+        auth = FirebaseAuth.getInstance();
+
+        // Login Button
+        LoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email_text = userETLogin.getText().toString();
+                String pass_text  = passETLogin.getText().toString();
+
+                // Checking if it is empty
+                if (TextUtils.isEmpty(email_text) || TextUtils.isEmpty(pass_text)) {
+                    Toast.makeText(Login_Activity.this, "Please fill the fields", Toast.LENGTH_SHORT);
+                } else {
+                    auth.signInWithEmailAndPassword(email_text, pass_text)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Intent i = new Intent(Login_Activity.this, MainActivity.class);
+                                        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(i);
+                                        finish();
+                                    } else {
+                                        Toast.makeText(Login_Activity.this, "Login Failed", Toast.LENGTH_SHORT);
+                                    }
+                                }
+                            });
+                }
+            }
+        });
     }
 }
